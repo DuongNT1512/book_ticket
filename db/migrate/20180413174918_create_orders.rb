7444262c3a35cbbc3cdfcbef6b210aa462048e19
Nam Chu <h.nam94@gmail.com>
2018-04-18 11:06:50 +0700

    Model and migration

---
+++ db/migrate/20180413174918_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.float :amount, null: false
+      t.integer :status, null: false
+      t.belongs_to :user, index: true
+      t.belongs_to :promotion, index: true
+
+      t.timestamps
+    end
+  end
+end
